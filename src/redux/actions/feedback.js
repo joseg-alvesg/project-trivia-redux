3f858ee22841d61d7053b2cf397b1223d3618c64
GrYllO <gryllo@tibiabr.com>
2023-02-10 01:21:49 -0300

    commented `RESTART_GAME` action;

--- src/redux/actions/feedback.js
+++ src/redux/actions/feedback.js
@@ -1,11 +1,12 @@
-import { GAME_RESULT, RESTART_GAME } from '../../constants';
+import { GAME_RESULT/* , RESTART_GAME */ } from '../../constants';
 
 export const gameResult = (payload) => ({
   type: GAME_RESULT,
   payload,
 });
 
-export const restartGame = (payload) => ({
+//? provavelmente será apenas uma função de redirect ao clicar no botão?
+/* export const restartGame = (payload) => ({
   type: RESTART_GAME,
   payload,
-});
+}); */

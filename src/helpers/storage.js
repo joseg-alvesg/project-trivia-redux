702bbc08b818ecade693bd1b06436e2fbb09dc12
GrYllO <gryllo@tibiabr.com>
2023-02-10 20:14:27 -0300

    added `localStorage` helpers to delete stuff;

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -3,7 +3,17 @@ export const saveToken = (token) => {
   localStorage.setItem('token', token);
 };
 
-export const saveToLocalStorage = (key, value) => localStorage
-  .setItem(key, JSON.stringify(value));
+export const saveToLocalStorage = (key, item) => localStorage
+  .setItem(key, JSON.stringify(item));
 
 export const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+
+export const deleteKeyFromLocalStorage = (key) => localStorage.removeItem(key);
+
+export const deleteArrayItemFromLocalStorage = (key, item) => {
+  const savedKey = localStorage.getItem(key);
+  if (!savedKey) return;
+  let items = JSON.parse(savedKey);
+  items = items.filter((i) => i !== item);
+  localStorage.setItem(key, JSON.stringify(items));
+};

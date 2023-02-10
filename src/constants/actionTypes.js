24f73e40cedc63e816bd305ba2f84de4542e2d54
GrYllO <gryllo@tibiabr.com>
2023-02-10 01:06:55 -0300

    added `FETCH_SESSION_TOKEN` consts; added comments for `END_GAME`, `NEXT_QUESTION`;

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,11 +1,14 @@
 export const ADD_COUNTER = 'ADD_COUNTER';
 export const ADD_POINT = 'ADD_POINT';
-export const END_GAME = 'END_GAME';
+export const END_GAME = 'END_GAME'; /* possivelmente desnecessário */
 export const FETCH_QUESTION_ERROR = 'FETCH_QUESTION_ERROR';
 export const FETCH_QUESTION_LOADING = 'FETCH_QUESTION_LOADING';
 export const FETCH_QUESTION_SUCCESS = 'FETCH_QUESTION_SUCCESS';
+export const FETCH_SESSION_TOKEN_ERROR = 'FETCH_SESSION_TOKEN_ERROR';
+export const FETCH_SESSION_TOKEN_LOADING = 'FETCH_SESSION_TOKEN_LOADING';
+export const FETCH_SESSION_TOKEN_SUCCESS = 'FETCH_SESSION_TOKEN_SUCCESS';
 export const GAME_RESULT = 'GAME_RESULT';
-export const NEXT_QUESTION = 'NEXT_QUESTION';
+export const NEXT_QUESTION = 'NEXT_QUESTION'; /* possivelmente desnecessário */
 export const QUESTION_RESULT = 'QUESTION_RESULT';
 export const RESTART_GAME = 'RESTART_GAME';
 export const SAVE_LOGIN = 'SAVE_LOGIN';

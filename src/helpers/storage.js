40b22b0bf018b3bf70594db486a8ad07f2e7c667
GrYllO <gryllo@tibiabr.com>
2023-02-10 19:25:53 -0300

    implements `localStorage` generic functions; added `index` to `helpers`;

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,3 +1,9 @@
+// TODO: substituir o uso dessa função pela saveToLocalStorage na aplicação
 export const saveToken = (token) => {
   localStorage.setItem('token', token);
 };
+
+export const saveToLocalStorage = (key, value) => localStorage
+  .setItem(key, JSON.stringify(value));
+
+export const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));

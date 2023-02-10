49c025c51e91530362789124c300d152716f02df
Samuel Alves <samucahhh@gmail.com>
2023-02-10 17:44:27 -0300

    implements function getPlayer in componentDidMount

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { MD5 } from 'crypto-js';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   state = {
     img: '',
     name: '',
     score: '',
+  };
+
+  componentDidMount() {
+    this.getPlayer();
+  }
+
+  getPlayer = () => {
+    const { player } = this.props;
+    const { name, gravatarEmail, score } = player;
+    this.setState({
+      name,
+      score,
+      img: MD5(gravatarEmail).toString(),
+    });
   };
 
   render() {

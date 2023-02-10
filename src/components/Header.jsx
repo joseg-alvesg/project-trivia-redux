c6b7820d7ad5a43fb9886ec8136998ab3570cbe8
Samuel Alves <samucahhh@gmail.com>
2023-02-10 17:23:45 -0300

    create header basic component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MD5 } from "crypto-js";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+
+export default (Header);

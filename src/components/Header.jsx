3c3c5541efa7eb0371615f40871a1f120a3870f8
Samuel Alves <samucahhh@gmail.com>
2023-02-10 17:34:22 -0300

    setting initial state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,20 @@ import { MD5 } from 'crypto-js';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  state = {
+    img: '',
+    name: '',
+    score: '',
+  };
+
   render() {
+    const { img, name, score } = this.state;
     return (
-      <div />
+      <div>
+        <img src={ img } alt={ name } />
+        <p>{name}</p>
+        <p>{score}</p>
+      </div>
     );
   }
 }

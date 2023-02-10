03f6778fccb85ee588c5295e568638113214eb40
GrYllO <gryllo@tibiabr.com>
2023-02-10 18:09:12 -0300

    added `/game` route;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 
@@ -9,6 +10,7 @@ export default function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
       </Switch>
     </div>

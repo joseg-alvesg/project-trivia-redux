9683b74dcf1fc017ab925d4cf5a1add361e3a8e8
GrYllO <gryllo@tibiabr.com>
2023-02-10 00:20:26 -0300

    added stub for `INITIAL_STATE`;

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
 export * from './actionTypes';
+/* export { default as initialState } from './initialState'; */
---
+++ src/constants/initialState.js
@@ -0,0 +1,18 @@
+/* const INITIAL_STATE = {
+  token: '',
+  player: {
+    name: '',
+    gravatarEmail: '',
+    score: 0,
+    assertions: 0,
+  },
+  ranking: [
+    {
+      name: '',
+      score: 0,
+      picture: '',
+    },
+  ],
+};
+
+export default INITIAL_STATE; */

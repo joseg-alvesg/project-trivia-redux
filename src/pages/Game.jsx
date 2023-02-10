48a9f1f0c96f98cbdebad142735950335a53bb68
GrYllO <gryllo@tibiabr.com>
2023-02-10 20:34:39 -0300

    implements 'Game' component fetchAPI logic;

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,16 +1,48 @@
+import { shape, func } from 'prop-types';
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+// TODO: descomentar componente quando implementado
+import { /* Header, */ Question } from '../components';
+import { questionApiRequest } from '../helpers/services';
+import {
+  deleteKeyFromLocalStorage,
+  getFromLocalStorage,
+} from '../helpers/storage';
+// ? import { connect } from 'react-redux';
+
+const CODE_TOKEN_NOT_FOUND = 3;
+const LOGIN_ROUTE = '/';
+const TOKEN_KEY = 'token';
 
 class Game extends Component {
+  componentDidMount() {
+    this.fetchQuestions();
+  }
+
+  fetchQuestions = async () => {
+    const { history } = this.props;
+    const token = getFromLocalStorage(TOKEN_KEY);
+    const questionsData = await questionApiRequest(token);
+    if (questionsData.response_code === CODE_TOKEN_NOT_FOUND) {
+      history.push(LOGIN_ROUTE);
+      deleteKeyFromLocalStorage(TOKEN_KEY);
+    }
+    this.setState({ ...questionsData });
+  };
+
   render() {
     return (
-      <div>Game</div>
+      <div>
+        {/* <Header /> */}
+        <Question { ...this.state } />
+      </div>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({});
+Game.propTypes = { history: shape({ push: func }) }.isRequired;
 
-// const mapDispatchToProps = {};
+// ? const mapStateToProps = (state) => ({});
 
-export default /* connect(mapStateToProps, mapDispatchToProps)( */Game/* ) */;
+// ? const mapDispatchToProps = {};
+
+export default /* connect(mapStateToProps, mapDispatchToProps)( */ Game;

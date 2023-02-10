d901130b59c4f750117a0b4cc6c85171b5908bbd
joseg-alvesg <cinhoalves85@gmail.com>
2023-02-10 16:09:35 -0300

    implements `sessionTokenThunk` action inplements `Login cases`

--- src/redux/reducers/login.js
+++ src/redux/reducers/login.js
@@ -1,11 +1,22 @@
+import { FETCH_SESSION_TOKEN_LOADING } from '../../constants';
 import INITIAL_STATE from '../../constants/initialState';
 
 const saveLoginReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case FETCH_SESSION_TOKEN_LOADING:
+    return {
+      ...state,
+      isLoading: true,
+    };
+  case FETCH_SESSION_TOKEN_SUCCESS:
+    return {
+      ...state,
+      token: action.payload,
+    };
   case SAVE_LOGIN:
     return {
       ...state,
-      ...action.payload,
+      player: { ...state.player, name: action.payload },
     };
   default:
     return state;

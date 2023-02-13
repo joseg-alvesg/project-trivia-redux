86061fc3015d485598a2669ac8fff47cfbaf0852
GrYllO <gryllo@tibiabr.com>
2023-02-13 15:35:39 -0300

    added basic routes;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Ranking from './pages/Ranking';
 import Settings from './pages/Settings';
 
 export default function App() {
@@ -10,9 +13,11 @@ export default function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/game" component={ Game } />
-        <Route path="/settings" component={ Settings } />
         <Route path="/game" component={ Game } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
+        <Route path="/settings" component={ Settings } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </div>
   );

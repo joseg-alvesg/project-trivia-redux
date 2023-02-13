b53cee94e5d1381adb1283fd9c825c67e436f7ac
Samuel Alves <samucahhh@gmail.com>
2023-02-13 18:01:39 -0300

    get ranking of the global state and add function map

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
+    const { ranking } = this.props;
     return (
       <div>
         <Link to="/" data-testid="btn-go-home">Jogar novamente</Link>
         <ul>
-          
+          { ranking.map((item, index) => (
+            <li key={ index }>
+              <p data-testid={ `player-name-${index}` }>{item.name}</p>
+              <p data-testid={ `player-score-${index}` }>{item.score}</p>
+            </li>
+          )) }
         </ul>
       </div>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  ranking: state.triviaReducer.ranking,
+});
 
-// const mapDispatchToProps = {};
-
-export default/*  connect(mapStateToProps, mapDispatchToProps)( */Ranking/* ) */;
+export default connect(mapStateToProps)(Ranking);

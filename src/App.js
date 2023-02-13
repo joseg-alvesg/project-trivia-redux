88a3700137600d8bbac06769eaac4f0accc4b918
Samuel Alves <samucahhh@gmail.com>
2023-02-13 13:04:37 -0300

    creates route game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/settings" component={ Settings } />
+        <Route path="/game" component={ Game } />
       </Switch>
     </div>
   );
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 
 class Settings extends React.Component {
   render() {
     return (
       <div>
-        <Header />
         <h1 data-testid="settings-title">Configurações</h1>
       </div>
     );

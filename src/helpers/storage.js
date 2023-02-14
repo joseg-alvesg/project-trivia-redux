f0d9bbd89bb069b09b4dba2c35d4d0540f23f720
GrYllO <gryllo@tibiabr.com>
2023-02-14 15:42:49 -0300

    updated `local storage` functions to address `token` case;

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,5 +1,10 @@
+import { TOKEN_KEY } from '../constants';
+
 export const saveToLocalStorage = (key, item) => {
   try {
+    if (typeof item === 'string') {
+      return localStorage.setItem(key, item);
+    }
     localStorage.setItem(key, JSON.stringify(item));
   } catch (error) {
     console.error(`Error saving item to local storage: ${error}`);
@@ -9,6 +14,9 @@ export const saveToLocalStorage = (key, item) => {
 export const getFromLocalStorage = (key) => {
   const item = localStorage.getItem(key);
   try {
+    if (key === TOKEN_KEY) {
+      return item ?? '';
+    }
     return item ? JSON.parse(item) : null;
   } catch (error) {
     console.error(`Error parsing item from local storage: ${error}`);

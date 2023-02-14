0e81a54a3a35bca5fd13bee6cddd569e80d9cacb
GrYllO <gryllo@tibiabr.com>
2023-02-14 02:17:23 -0300

    refactored `Header` due to redux refactoring;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import { MD5 } from 'crypto-js';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { GRAVATAR_ENDPOINT } from '../constants';
 
 class Header extends React.Component {
-  state = {
-    img: '',
-    name: '',
-    score: '',
-  };
-
-  componentDidMount() {
-    this.getPlayer();
-  }
-
-  getPlayer = () => {
-    const { player } = this.props;
-    const { name, gravatarEmail, score } = player;
-    this.setState({
-      name,
-      score,
-      img: MD5(gravatarEmail).toString(),
-    });
-  };
-
   render() {
-    const { img, name, score } = this.state;
+    const { gravatarEmail, name, score } = this.props;
     return (
       <div>
-        <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${img}` } alt={ name } />
+        <img
+          data-testid="header-profile-picture"
+          src={ `${GRAVATAR_ENDPOINT}${gravatarEmail}` }
+          alt={ name }
+        />
         <p data-testid="header-player-name">{name}</p>
         <p data-testid="header-score">{score}</p>
       </div>
@@ -36,16 +20,16 @@ class Header extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  player: state.saveLoginReducer.player,
-});
-
 Header.propTypes = {
-  player: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    gravatarEmail: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired,
-  }).isRequired,
-};
+  gravatarEmail: PropTypes.string,
+  name: PropTypes.string,
+  score: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = ({ player: { gravatarEmail, name, score } }) => ({
+  gravatarEmail,
+  name,
+  score,
+});
 
 export default connect(mapStateToProps)(Header);

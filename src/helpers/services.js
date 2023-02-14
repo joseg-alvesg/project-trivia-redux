d104b5b295c200a6aa0aad77238c55466aa6f337
GrYllO <gryllo@tibiabr.com>
2023-02-14 02:17:53 -0300

    fix services due to redux refactoring;

--- src/helpers/services.js
+++ src/helpers/services.js
@@ -1,13 +1,13 @@
 import { SESSION_TOKEN_ENDPOINT, QUESTIONS_ENDPOINT } from '../constants';
 
-export const tokenApiRequest = async () => {
+export const requestToken = async () => {
   const request = await fetch(SESSION_TOKEN_ENDPOINT);
   const response = await request.json();
   return response;
 };
 
-export const questionApiRequest = async (token) => {
-  const request = await fetch(QUESTIONS_ENDPOINT(token));
+export const requestQuestions = async (token) => {
+  const request = await fetch(`${QUESTIONS_ENDPOINT}${token}`);
   const response = await request.json();
   return response;
 };

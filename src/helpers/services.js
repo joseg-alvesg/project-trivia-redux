1622ed7185a2156cfa05aa0e0ef2fab61afb733d
GrYllO <gryllo@tibiabr.com>
2023-02-14 14:44:38 -0300

    added error handling;

--- src/helpers/services.js
+++ src/helpers/services.js
@@ -1,13 +1,23 @@
-import { SESSION_TOKEN_ENDPOINT, QUESTIONS_ENDPOINT } from '../constants';
+import { QUESTIONS_ENDPOINT, SESSION_TOKEN_ENDPOINT } from '../constants';
 
 export const requestToken = async () => {
-  const request = await fetch(SESSION_TOKEN_ENDPOINT);
-  const response = await request.json();
-  return response;
+  try {
+    const request = await fetch(SESSION_TOKEN_ENDPOINT);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.error('Error requesting session token:', error);
+    throw error;
+  }
 };
 
 export const requestQuestions = async (token) => {
-  const request = await fetch(`${QUESTIONS_ENDPOINT}${token}`);
-  const response = await request.json();
-  return response;
+  try {
+    const request = await fetch(`${QUESTIONS_ENDPOINT}${token}`);
+    const response = await request.json();
+    return response;
+  } catch (error) {
+    console.error('Error requesting questions:', error);
+    throw error;
+  }
 };

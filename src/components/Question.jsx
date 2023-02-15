7b2d8c187fd8726c6ef6cee8533e337a73527fe0
joseg-alvesg <cinhoalves85@gmail.com>
2023-02-15 12:51:29 -0300

    increments `handleCkick` to stop timer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,26 +2,35 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import handleSort from '../helpers/gameFunctions';
+import { stopTimer } from '../redux/actions/gameActions';
+import Timer from './Timer';
 
 class Question extends Component {
+  hancleClick = () => {
+    const { dispatch } = this.props;
+    dispatch(stopTimer());
+  };
+
   render() {
-    const { questions, assertions } = this.props;
+    const { questions, counter, timeRunning } = this.props;
     return (
       <main>
         <section>
-          <p data-testid="question-category">{questions[assertions].category}</p>
-          <div data-testid="question-text">{questions[assertions].question}</div>
+          <p data-testid="question-category">{questions[counter].category}</p>
+          <div data-testid="question-text">{questions[counter].question}</div>
         </section>
         <section>
-          <div>Timer HERE (as component?)</div>
+          <Timer />
         </section>
         <section>
           <div>
-            {handleSort(questions[assertions]).map((quest, index) => (
+            {handleSort(questions[counter]).map((quest, index) => (
               <section key={ index } data-testid="answer-options">
                 <button
                   type="button"
-                  data-testid={ questions[assertions].correct_answer === quest
+                  onClick={ this.hancleClick }
+                  disabled={ !timeRunning }
+                  data-testid={ questions[counter].correct_answer === quest
                     .answer ? 'correct-answer' : `wrong-answer${index}` }
                 >
                   {quest.answer}
@@ -36,13 +45,15 @@ class Question extends Component {
 }
 
 Question.propTypes = {
-  assertions: PropTypes.number,
+  counter: PropTypes.number,
   questions: PropTypes.objectOf(PropTypes.any),
 }.isRequired;
 
 const mapStateToProps = (state) => ({
   questions: state.game.results,
-  assertions: state.player.assertions,
+  counter: state.game.counter,
+  timer: state.game.timer,
+  timeRunning: state.game.timeRunning,
 });
 
 // const mapDispatchToProps = {};

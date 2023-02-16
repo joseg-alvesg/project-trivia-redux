5e4a1d906fe7c7482617bb5abc03a21a77f19e23
joseg-alvesg <cinhoalves85@gmail.com>
2023-02-16 12:25:20 -0300

    inplements more tests to imporve coverage

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,9 +1,12 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import {requestToken} from '../helpers/services';
 
- describe('Testando a página de Login', () => {
+const GRAVATAR_EMAIL = 'fenrock@triviaemail.com'
+
+ describe.only('Testando a página de Login', () => {
   it('verifica se os elementos estão presentes na tela de login', () => {
     renderWithRouterAndRedux(<App />);
 
@@ -24,7 +27,7 @@ import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
     renderWithRouterAndRedux(<App />);
     
     const inputGravatarEmail = screen.getByTestId('input-gravatar-email');
-    userEvent.type(inputGravatarEmail, 'fenrock@triviamail.com');
+    userEvent.type(inputGravatarEmail, GRAVATAR_EMAIL);
   });
 
   it('verifica se o botão de jogar está desativado enquanto não tem nada preenchido nos inputs', () => {
@@ -36,8 +39,49 @@ import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
     const inputPlayerName = screen.getByTestId('input-player-name');
     const inputGravatarEmail = screen.getByTestId('input-gravatar-email');
     userEvent.type(inputPlayerName, 'Fenrock');
-    userEvent.type(inputGravatarEmail, 'fenrock@triviaemail.com');
+    userEvent.type(inputGravatarEmail, GRAVATAR_EMAIL);
     expect(btnPlay).toBeEnabled();
   });
+
+  it('verifica se o botão de jogar está habilitado quando os inputs estão preenchidos', async () => {
+    const {history} = renderWithRouterAndRedux(<App />);
+
+    const btnPlay = screen.getByTestId('btn-play');
+    const inputPlayerName = screen.getByTestId('input-player-name');
+    const inputGravatarEmail = screen.getByTestId('input-gravatar-email');
+    userEvent.type(inputPlayerName, 'Fenrock');
+    userEvent.type(inputGravatarEmail, GRAVATAR_EMAIL);
+    expect(btnPlay).toBeEnabled();
+
+    userEvent.click(btnPlay);
+    await waitFor(() => {
+      history.push('/game');
+      expect(screen.getByTestId('header-player-name')).toBeInTheDocument();
+      expect(screen.getByTestId('header-score')).toBeInTheDocument();
+      expect(screen.getByTestId('question-text')).toBeInTheDocument();
+    });
+
+    expect(inputPlayerName).not.toBeInTheDocument();
+    
+  });
+
+  it('Verifica se as informações de usuario são salvas no estado global', async () => {
+    const {history, store} = renderWithRouterAndRedux(<App />);
+
+    const btnPlay = screen.getByTestId('btn-play');
+    const inputPlayerName = screen.getByTestId('input-player-name');
+    const inputGravatarEmail = screen.getByTestId('input-gravatar-email');
+    userEvent.click(inputPlayerName)
+    userEvent.type(inputPlayerName, 'Fenrock');
+    userEvent.click(inputGravatarEmail)
+    userEvent.type(inputGravatarEmail, GRAVATAR_EMAIL);
+    expect(btnPlay).toBeEnabled();
+
+    userEvent.click(btnPlay);
+
+    await waitFor(() => {
+      const { player } = store.getState();
+      expect(player.name).toBe('Fenrock');
+    });
+  });
  });
- 

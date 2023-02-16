af552ccbc0811e6a09d686825f5a08e1abbcda64
Samuel Alves <samucahhh@gmail.com>
2023-02-16 01:00:02 -0300

    connect component

--- src/components/PlayAgainButton.jsx
+++ src/components/PlayAgainButton.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { playAgain } from '../redux/actions/gameActions';
 
 class PlayAgainButton extends Component {
   render() {
+    const { dispatch } = this.props;
     return (
-      <Link data-testid="btn-play-again" to="/">Jogar novamente</Link>
+      <Link
+        data-testid="btn-play-again"
+        to="/"
+        onClick={ () => dispatch(playAgain()) }
+      >
+        Jogar novamente
+
+      </Link>
     );
   }
 }
 
-export default PlayAgainButton;
+export default connect()(PlayAgainButton);

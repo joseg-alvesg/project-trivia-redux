59d555e1d38322ccdd2443641649ca061928449d
joseg-alvesg <cinhoalves85@gmail.com>
2023-02-15 22:38:46 -0300

    increments `DIFICULTY_POINTS and `SCORE_SUM`

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,5 @@ export const MIN_FEEDBACK_SCORE = 3;
 export const SORT_FACTOR = 0.5;
 export const TOKEN_KEY = 'token';
 export const TIMER_INTERVAL = 1000;
+export const DIFICULTY_POINTS = { hard: 3, medium: 2, easy: 1 };
+export const SCORE_SUM_VALUE = 10;
